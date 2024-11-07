26e31accd0df0a27fef8b2b5accaaeca20ed3096
Kevin Potter <kvn.potter@outlook.com>
2024-11-07 13:22:15 +0100

    Work on testing functions

--- utils/SaveFunction.py
+++ utils/SaveFunction.py
@@ -2,6 +2,8 @@
 
 import json
 from datetime import datetime
+import pytest
+import os
 
 # Functions
 
@@ -13,3 +15,15 @@ def save(leaders_per_country: dict) -> None:
     filename = './results/leaders.json'
     with open(filename, "w") as output:
         output.write(now + "/n" + json.dumps(leaders_per_country))
+
+# Testing functions
+
+def test_save():
+    save({'Empty':'Dict'})
+    assert os.path.exists('./results/leaders.json')
+    with open('./results/leaders.json', 'r') as file:
+              text = file.read()
+              assert str(datetime.today().date()) == text[:10]
+
+if __name__ = "__main__":
+    pytest.main(['-v'])
